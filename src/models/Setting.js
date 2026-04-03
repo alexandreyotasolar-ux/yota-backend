a76b92e9d8f3c580a3780a3b99e2a5eae2eb847f
basecomercialyota <alexandreyotasolar@gmail.com>
2026-04-03 15:28:36 -0300

    Add files via upload

---
+++ src/models/Setting.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const settingSchema = new mongoose.Schema({
+  key: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true
+  },
+  value: {
+    type: mongoose.Schema.Types.Mixed,
+    required: true
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+settingSchema.pre('save', function (next) {
+  this.updatedAt = new Date();
+  next();
+});
+
+module.exports = mongoose.model('Setting', settingSchema);
